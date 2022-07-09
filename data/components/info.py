bc391030db056697c7955c03951f96cd0aca1952
VergilA <46660335+VergilA@users.noreply.github.com>
2022-07-09 11:12:31 +0800

    载入游戏

--- data/components/info.py
+++ data/components/info.py
@@ -30,7 +30,7 @@ class Info:
         elif self.state == C.LOAD_SCREEN:
             self.state_labels[(self.create_label('WORLD'))] = (280,200)
             self.state_labels[(self.create_label('1-1'))] = (430,200)
-            self.state_labels[(self.create_label('X     3'))] = (380,280)
+            self.state_labels[(self.create_label('X   3'))] = (380,280)
             self.player_image = tools.get_image(setup.GRAPHICS['mario_bros'],178,32,12,16,(0,0,0),C.BG_MULTI)
 
     # 用于创建通用分数、金币数等信息
