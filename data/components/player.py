3c9390d0fb38d2822d138f79e8d0daaf28ef094b
VergilA <46660335+VergilA@users.noreply.github.com>
2022-11-13 23:47:25 +0800

    批量换肤

--- data/components/player.py
+++ data/components/player.py
@@ -1,20 +1,31 @@
 import pygame
 from data import tools,setup
 from data import constants as C
+import json
+import os
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,name):
         pygame.sprite.Sprite.__init__(self)
         self.name = name
+        self.load_data()
         self.setup_states()
         self.setup_velocities()
         self.setup_timers()
         self.load_images()
 
+    def load_data(self):
+        file_name = self.name+'.json'
+        file_path = os.path.join('config/player',file_name)
+        with open(file_path) as f:
+            self.player_data = json.load(f)
 
     # 设置主角状态
     def setup_states(self):
-        pass
+        self.state = ''
+        self.face_right = True
+        self.dead = False
+        self.big = False
 
     # 主角速度设置
     def setup_velocities(self):
@@ -31,23 +42,56 @@ class Player(pygame.sprite.Sprite):
 
     def load_images(self):
         sheet = setup.GRAPHICS['mario_bros']
-        self.right_frames = []
-        self.left_frames = []
-
         # 运动的4个帧信息
-        frame_rects = [
-            (178,32,12,16),
-            (80,32,15,16),
-            (96,32,12,16),
-            (112,32,12,16),
+        frame_rects = self.player_data['image_frames']
+
+        self.right_small_normal_frames = []
+        self.right_big_normal_frames = []
+        self.right_big_fire_frames = []
+
+        self.left_small_normal_frames = []
+        self.left_big_normal_frames = []
+        self.left_big_fire_frames = []
+
+        self.small_normal_frames = [self.right_small_normal_frames,self.left_small_normal_frames]
+        self.big_normal_frames = [self.right_big_normal_frames,self.left_big_normal_frames]
+        self.big_fire_frames = [self.right_big_fire_frames,self.left_big_fire_frames]
+
+        self.all_frames = [
+            self.right_small_normal_frames,
+            self.right_big_normal_frames,
+            self.right_big_fire_frames,
+            self.left_small_normal_frames,
+            self.left_big_normal_frames,
+            self.left_big_fire_frames
         ]
 
-        for frame_rect in frame_rects:
-            right_image = tools.get_image(sheet,*frame_rect,(0,0,0),C.PLAYER_MULTI)
-            left_image = pygame.transform.flip(right_image,True,False)
+        # 默认左和右的帧库
+        self.right_frames = self.right_small_normal_frames
+        self.left_frames = self.left_small_normal_frames
 
-            self.right_frames.append(right_image)
-            self.left_frames.append(left_image)
+        for group, group_frame_rects in frame_rects.items():
+            for frame_rect in group_frame_rects:
+                right_image = tools.get_image(
+                    sheet,
+                    frame_rect['x'],
+                    frame_rect['y'],
+                    frame_rect['width'],
+                    frame_rect['height'],
+                    (0,0,0,0),
+                    C.PLAYER_MULTI)
+                left_image = pygame.transform.flip(right_image,True,False)
+
+                # 根据名称添加到对应帧库
+                if group == 'right_small_normal':
+                    self.right_small_normal_frames.append(right_image)
+                    self.left_small_normal_frames.append(left_image)
+                if group == 'right_big_normal':
+                    self.right_big_normal_frames.append(right_image)
+                    self.left_big_normal_frames.append(left_image)
+                if group == 'right_big_fire':
+                    self.right_big_fire_frames.append(right_image)
+                    self.left_big_fire_frames.append(left_image)
 
         self.frame_index = 0
         self.frames = self.right_frames
@@ -58,19 +102,32 @@ class Player(pygame.sprite.Sprite):
     def update(self,keys):
         # 设置时间间隔，修改运动图片
         self.current_time = pygame.time.get_ticks()
+
         if keys[pygame.K_RIGHT]:
+            self.state = 'walk'
             self.x_vel = 5
             self.y_vel = 0
             self.frames = self.right_frames
+
         if keys[pygame.K_LEFT]:
+            self.state = 'walk'
             self.x_vel = -5
             self.y_vel = 0
             self.frames = self.left_frames
 
+        if keys[pygame.K_SPACE]:
+            self.state = 'jump'
+            self.y_vel = -5
+
         # 时间间隔大于100毫秒，切换下一张图片
-        if self.current_time - self.walking_timer > 100:
-            self.walking_timer = self.current_time
-            self.frame_index += 1
-            self.frame_index %= 4
+        if self.state == 'walk':
+            if self.current_time - self.walking_timer > 100:
+                self.walking_timer = self.current_time
+                self.frame_index += 1
+                self.frame_index %= 4
+
+        if self.state == 'jump':
+            self.frame_index = 4
+
         self.image = self.frames[self.frame_index]
 
